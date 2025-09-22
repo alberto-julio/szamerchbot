369c6744f2ee909c94e8ff4970c66deaae1a6a9e
Alberto <albertojulio@berkeley.edu>
2025-09-22 00:42:48 -0700

    started setting up web scrapper

--- scraper.py
+++ scraper.py
@@ -2,10 +2,23 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import re
+import time
+import logging
 
 sza_url = "https://txdxe.com/collections/sza"
 not_url = "https://www.notbeauty.com/"
+
+#TAKE A GRAND TOUR GRAND TOUR DRAGON BALL GT!!!!!!!!
 grand_tour_url = "https://shop.grandnationaltour.com/"
+
+
+def fetch_page(url: str) -> str:
+
+    headers = {}
+    response = requests.get(url, headers=headers)
+    raw_html = response.text
+    return raw_html
+
 
 class Scraper:
     def __init__(self, site, selectors, stock_indicators, scraper_settings):
